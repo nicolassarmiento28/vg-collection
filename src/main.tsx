c75d307815638e3da3638b754ab57da8994bc390
Nicolas Sarmiento <nicolas@example.com>
2026-04-13 20:43:24 -0400

    feat: apply Ant Design dark ember theme via ConfigProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App as AntdApp, ConfigProvider } from 'antd'
+import { App as AntdApp, ConfigProvider, theme } from 'antd'
 import './index.css'
 import App from './App.tsx'
 import { GamesProvider } from './features/games/state/GamesContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ConfigProvider>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          colorPrimary: '#e03c2f',
+          colorBgContainer: '#1a1918',
+          colorBgElevated: '#242220',
+          colorBorder: '#2e2b28',
+          colorText: '#c9c2b8',
+          colorTextHeading: '#f5f0ea',
+          fontFamily: "'DM Sans', sans-serif",
+          borderRadius: 6,
+          colorLink: '#e03c2f',
+        },
+      }}
+    >
       <AntdApp>
         <GamesProvider>
           <App />
